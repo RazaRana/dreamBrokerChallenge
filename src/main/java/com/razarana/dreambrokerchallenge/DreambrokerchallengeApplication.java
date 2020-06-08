93caaf2e9005323bfbc9dd369c6606c5603647db
Raza Rana <raza.rana.khan@gmail.com>
2020-06-08 15:21:58 +0300

    project created

---
+++ src/main/java/com/razarana/dreambrokerchallenge/DreambrokerchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.razarana.dreambrokerchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreambrokerchallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreambrokerchallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/razarana/dreambrokerchallenge/DreambrokerchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.razarana.dreambrokerchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreambrokerchallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
