066544405e3ccd3758cf7c517386d5f25baa6bb7
Raza Rana <raza.rana.khan@gmail.com>
2020-06-08 16:33:52 +0300

    service added

--- src/main/java/com/razarana/dreambrokerchallenge/Controller/Analyze.java
+++ src/main/java/com/razarana/dreambrokerchallenge/Controller/Analyze.java
@@ -1,11 +1,16 @@
 package com.razarana.dreambrokerchallenge.Controller;
 
+import com.razarana.dreambrokerchallenge.Service.AnalysisService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Analyze {
+
+    @Autowired
+    AnalysisService analysisService;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String welcomeMessage() {
@@ -14,6 +19,6 @@ public class Analyze {
 
     @PostMapping("/analyze")
     public String analysisString(@RequestBody String data) {
-        return "";
+        return analysisService.analyzeData(data);
     }
 }
---
+++ src/main/java/com/razarana/dreambrokerchallenge/Service/AnalysisService.java
@@ -0,0 +1,12 @@
+package com.razarana.dreambrokerchallenge.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnalysisService {
+
+    public String analyzeData(String data){
+
+        return "";
+    }
+}
