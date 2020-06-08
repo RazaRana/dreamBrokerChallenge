52cce97513048e0b4a917a2b11aee5861fd17410
Raza Rana <raza.rana.khan@gmail.com>
2020-06-09 02:07:57 +0300

    curl command problem fix

--- src/main/java/com/razarana/dreambrokerchallenge/Controller/AnalysisController.java
+++ src/main/java/com/razarana/dreambrokerchallenge/Controller/AnalysisController.java
@@ -1,5 +1,7 @@
 package com.razarana.dreambrokerchallenge.Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.razarana.dreambrokerchallenge.Model.AnalysisResult;
 import com.razarana.dreambrokerchallenge.Service.AnalysisService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,14 @@ public class AnalysisController {
 
     @PostMapping("/analyze")
     public AnalysisResult analysisString(@RequestBody String text) {
+
+        //to fix this curl command problem of double quotes
+        text=text.replace("'{text:","");
+        text=text.replace("}'","");
+
+
         return analysisService.analyzeData(text);
+
+
     }
 }
